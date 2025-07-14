b8894f1fd7ae0e1e4541583285704c2604563195
Cameron Zemek <contact@cameronzemek.com>
2025-07-14 23:59:27 +1000

    Forgot to update fasta/setup.sh

--- fasta/setup.sh
+++ fasta/setup.sh
@@ -7,6 +7,6 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 if [[ ! -s "$RAMDISK/test25M.txt" ]]; then
   echo "Generating fasta test25M.txt"
   (cd $SCRIPT_DIR/go; ./build.sh)
-  $SCRIPT_DIR/go/fasta 25000000 > $RAMDISK/test25M.txt
+  $SCRIPT_DIR/go/fasta 25000000 $RAMDISK/test25M.txt
 fi
 (cd $RAMDISK && md5sum -c $SCRIPT_DIR/test25M.txt.md5)
