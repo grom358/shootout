c23af43715ca17f6a1f03ddfe013508ad9a2f5b5
Cameron Zemek <contact@cameronzemek.com>
2026-07-05 17:10:18 +1000

    Fix lexer d_pgo build issue

--- lexer/d_pgo/build.sh
+++ lexer/d_pgo/build.sh
@@ -11,13 +11,13 @@ if [ ! -s $SCRIPT_DIR/test.prof ]; then
   check_file $INPUT
   check_file $EXPECTED
 
-  ldc2 -O3 -fprofile-generate=rawtest.prof zscript.d
+  ldc2 -O3 -fprofile-generate=rawtest.prof zscript.d -L/usr/lib/llvm21/lib/clang/21/lib/linux/libclang_rt.profile-x86_64.a
 
   bench d $EXPECTED $ACTUAL "$SCRIPT_DIR/zscript $INPUT $ACTUAL"
 
   check_file $SCRIPT_DIR/rawtest.prof
 
-  llvm-profdata-19 merge -output=test.prof rawtest.prof
+  llvm-profdata-21 merge -output=test.prof rawtest.prof
   rm $SCRIPT_DIR/rawtest.prof
 
   check_file $SCRIPT_DIR/test.prof
