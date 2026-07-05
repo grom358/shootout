769dd3773ce4ad45a7e3d8228a2f7b3684f90318
Cameron Zemek <contact@cameronzemek.com>
2026-07-05 19:43:19 +1000

    Fix performance issue with knucleotide D implementation doing case conversion when counting nucleotides

--- knucleotide/d/knucleotide.d
+++ knucleotide/d/knucleotide.d
@@ -7,7 +7,7 @@ int[string] countNucleotides(string data, size_t k) {
     int[string] counts;
     size_t endIndex = data.length - k;
     for (size_t i = 0; i <= endIndex; ++i) {
-        auto fragment = data[i .. i + k].toLower();
+        auto fragment = data[i .. i + k];
         counts[fragment]++;
     }
     return counts;
