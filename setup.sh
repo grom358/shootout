e2dc8c534fc8702479ca45766333f400b77fb54d
Cameron Zemek <contact@cameronzemek.com>
2026-07-05 17:10:41 +1000

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@ if [[ -z "$RAMDISK" ]]; then
   echo "Error: RAMDISK is not set"
   exit 1
 fi
-./fasta/setup.sh
-./mandelbrot/setup.sh
-./lexer/setup.sh
+(cd fasta && ./setup.sh)
+(cd mandelbrot && ./setup.sh)
+(cd lexer && ./setup.sh)
