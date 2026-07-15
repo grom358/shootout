d915a8c2d77cac2544c2c4b91b621d67f1425ecc
Cameron Zemek <contact@cameronzemek.com>
2026-07-16 09:32:03 +1000

    Use try-with-resource for OutputStream

--- lexer/java_custom/src/main/java/com/cameronzemek/zscript/Main.java
+++ lexer/java_custom/src/main/java/com/cameronzemek/zscript/Main.java
@@ -17,33 +17,33 @@ public class Main {
     byte[] bytes = Files.readAllBytes(Paths.get(inputFile));
     String input = new String(bytes);
 
-    OutputStream out = new BufferedOutputStream(new FileOutputStream(outputFile));
-
     Lexer lexer = new Lexer(input);
     Token token;
-    while ((token = lexer.getNext()).getType() != TokenType.EOF) {
-      String text = token.getText();
-      if (token.getType() == TokenType.STRING) {
-        text = text.substring(1, text.length() - 1);
+    try (OutputStream out = new BufferedOutputStream(new FileOutputStream(outputFile))) {
+      while ((token = lexer.getNext()).getType() != TokenType.EOF) {
+        String text = token.getText();
+        if (token.getType() == TokenType.STRING) {
+          text = text.substring(1, text.length() - 1);
+        }
+        if (token.getType() == TokenType.STRING ||
+        token.getType() == TokenType.COMMENT) {
+          text = text.replaceAll("\"", "\"\"");
+        }
+        // Its more natural to use PrintWriter and format string here. Eg.
+        // printWriter.printf("%d,%d,\"%s\",\"%s\"\n",
+        //                    token.getLineNo(), token.getColumnNo(),
+        //                    text, token.getType());
+        // However the format machinery is slow and PrintWriter uses locking.
+        out.write(Integer.toString(token.getLineNo()).getBytes());
+        out.write(",".getBytes());
+        out.write(Integer.toString(token.getColumnNo()).getBytes());
+        out.write(",\"".getBytes());
+        out.write(text.getBytes());
+        out.write("\",\"".getBytes());
+        out.write(token.getType().toString().getBytes());
+        out.write("\"\n".getBytes());
       }
-      if (token.getType() == TokenType.STRING ||
-          token.getType() == TokenType.COMMENT) {
-        text = text.replaceAll("\"", "\"\"");
-      }
-      // Its more natural to use PrintWriter and format string here. Eg.
-      // printWriter.printf("%d,%d,\"%s\",\"%s\"\n",
-      //                    token.getLineNo(), token.getColumnNo()
-      //                    text, token.getType());
-      // However the format machinery is slow and PrintWriter uses locking.
-      out.write(Integer.toString(token.getLineNo()).getBytes());
-      out.write(",".getBytes());
-      out.write(Integer.toString(token.getColumnNo()).getBytes());
-      out.write(",\"".getBytes());
-      out.write(text.getBytes());
-      out.write("\",\"".getBytes());
-      out.write(token.getType().toString().getBytes());
-      out.write("\"\n".getBytes());
+      out.flush();
     }
-    out.flush();
   }
 }
